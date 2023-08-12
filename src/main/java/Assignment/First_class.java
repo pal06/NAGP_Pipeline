f800b81d46f4f590cb81596c180e7c341ba75d1e
pal06 <palvikasinha@gmail.com>
2023-08-13 01:20:49 +0530

    first commit

---
+++ src/main/java/Assignment/First_class.java
@@ -0,0 +1,39 @@
+package Assignment;
+
+public class First_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		add();
+		subtract();
+		multiply();
+
+	}
+	
+	public static void add()
+	{
+		int a=10;
+		int b=20;
+		int result = a+b;
+		System.out.println("the value of a+b "+result);
+	}
+	
+	public static void subtract()
+	{
+		int a=50;
+		int b=30;
+		int result = a-b;
+		System.out.println("the value of a-b "+result);
+	}
+	
+	public static void multiply()
+	{
+		int a=30;
+		int b=30;
+		int result =a*b;
+		System.out.println("the value of a-b "+result);
+		
+	}
+
+}
---
+++ src/test/java/Suit/Test.java
@@ -0,0 +1,29 @@
+package Suit;
+
+
+import Assignment.First_class;
+
+
+
+public class Test {
+	
+	@org.testng.annotations.Test
+	public void Test1()
+	{
+		System.out.println("this is first test");
+	}
+	
+	@org.testng.annotations.Test
+	public void Test2()
+	{
+		System.out.println("this is my second report");
+	}
+	
+	@org.testng.annotations.Test
+	public void Test3()
+	{
+		System.out.println("this is third report");
+	}
+
+	
+}
